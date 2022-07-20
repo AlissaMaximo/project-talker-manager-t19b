62bb098fc065e1470ff4ea08bbbc1bc2affdb69d
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-07-20 10:05:31 -0300

    feat: requisito 5 feito

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const Joi = require('joi');
 
 const schema = Joi.object({
     email: Joi.string().email(),
-    
     password: Joi.string().min(6),
+    rate: Joi.number().integer().min(1).max(5), // https://www.tabnine.com/code/javascript/functions/joi/NumberSchema/integer
 });
 
 function validateUser(request, response, next) {
@@ -27,6 +27,78 @@ function validateUser(request, response, next) {
 
   next();
 }
+
+const validateToken = (request, response, next) => {
+  const { authorization } = request.headers;
+
+  if (!authorization) return response.status(401).json({ message: 'Token não encontrado' });
+  if (authorization.length !== 16) {
+    return response.status(401).json({ message: 'Token inválido' });
+  }
+
+  next();
+};
+
+const validateName = (request, response, next) => {
+  const { name } = request.body;
+
+  if (!name) return response.status(400).json({ message: 'O campo "name" é obrigatório' });
+  if (name.length < 3) {
+    return response.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+  }
+
+  next();
+};
+
+const validateAge = (request, response, next) => {
+  const { age } = request.body;
+
+  if (!age) return response.status(400).json({ message: 'O campo "age" é obrigatório' });
+  if (age < 18) {
+    return response.status(400).json({ message: 'A pessoa palestrante deve ser maior de idade' });
+  }
+
+  next();
+};
+
+const validateTalk = (request, response, next) => {
+  const { talk } = request.body;
+
+  if (!talk) return response.status(400).json({ message: 'O campo "talk" é obrigatório' });
+
+  next();
+};
+
+const validateView = (request, response, next) => {
+  const { talk: { watchedAt } } = request.body;
+  const regexDateFormat = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i;
+  // https://stackoverflow.com/questions/62960834/regex-date-dd-mm-yyyy
+
+  if (!watchedAt) {
+    return response.status(400).json({ message: 'O campo "watchedAt" é obrigatório' }); 
+  }
+  if (!regexDateFormat.test(watchedAt)) {
+    return response.status(400)
+    .json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
+  }
+
+  next();
+};
+
+const validateRate = (request, response, next) => {
+  const { talk: { rate } } = request.body;
+  const rateError = schema.validate({ rate }).error;
+
+  if (!rate && rate !== 0) {
+    return response.status(400).json({ message: 'O campo "rate" é obrigatório' }); 
+  }
+  if (rateError !== undefined) {
+    return response.status(400)
+    .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+  }
+
+  next();
+};
 
 const app = express();
 app.use(bodyParser.json());
@@ -62,27 +134,26 @@ app.get('/talker/:id', async (request, response) => {
 });
 
 app.post('/login', validateUser, async (request, response) => {
-  // const { email, password } = request.body;
-
   const token = crypto.randomBytes(8).toString('hex');
 
   return response.status(200).json({ token });
 });
 
+app.post('/talker',
+validateToken, validateName, validateAge, validateTalk, validateView, validateRate,
+async (request, response) => {
+  const talkers = JSON.parse(await fs.readFile('./talker.json'));
+  
+  const { name, age, talk } = request.body;
+  const id = talkers.length + 1;
+  const newTalker = { id, name, age, talk };
+
+  talkers.push(newTalker);
+
+  fs.writeFile('./talker.json', JSON.stringify(talkers));
+  return response.status(201).json(newTalker);
+});
+
 app.listen(PORT, () => {
   console.log('Online', PORT);
 });
-
-/* if (!email) {
-    return response.status(400).json({ message: 'O campo "email" é obrigatório' });
-  }
-  if (!password) {
-    return response.status(400).json({ message: 'O campo "password" é obrigatório' });
-  }
-  
-  if (email !== validUser.email) {
-    return response.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
-  }
-  if (password < 6) {
-    return response.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
-  } */
